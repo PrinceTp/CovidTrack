4fe00c77446d9eaada5a553325ad9571ed7ec1c6
PrinceTp <tprince1812@gmail.com>
2024-05-31 00:04:33 +0530

    data_update

--- src/main/java/io/datatracker/coronavirustracker/services/CoronaVirusDataService.java
+++ src/main/java/io/datatracker/coronavirustracker/services/CoronaVirusDataService.java
@@ -21,8 +21,7 @@ import java.util.ArrayList;
 @Service
 public class CoronaVirusDataService {
 
-    private static String VIRUS_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
-
+    private static String VIRUS_DATA_URL = "https://raw.githubusercontent.com/PrinceTp/CovidTrack/main/time_series_covid19_confirmed_global.csv";
 
     private List<LocationStats> allStats = new ArrayList<>();
 
