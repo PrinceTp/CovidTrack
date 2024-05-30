c91d8898f13b09f6f15e61fc6a9e2ef6c515fbfd
PrinceTp <tprince1812@gmail.com>
2024-05-30 11:41:21 +0530

    init

---
+++ src/main/java/io/datatracker/coronavirustracker/controllers/HomeController.java
@@ -0,0 +1,26 @@
+package io.datatracker.coronavirustracker.controllers;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import io.datatracker.coronavirustracker.services.CoronaVirusDataService;
+import io.datatracker.coronavirustracker.models.LocationStats;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    CoronaVirusDataService coronaVirusDataService;
+
+    @GetMapping("/")
+    public String home(Model model) {
+        List<LocationStats> allStats = coronaVirusDataService.getAllStats();
+        int totalReportedCases = allStats.stream().mapToInt(stat -> stat.getLatestTotalCases()).sum();
+        model.addAttribute("locationStats", allStats);
+        model.addAttribute("totalReportedCases", allStats);
+
+        return "home";
+    }
+}
